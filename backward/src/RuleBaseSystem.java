e045d71326c3eae033799419628267bffbdbe4cc
hirachin <hirachin0113@gmail.com>
2018-10-29 14:20:16 +0900

    ArrayListの<>を追加

--- backward/src/RuleBaseSystem.java
+++ backward/src/RuleBaseSystem.java
@@ -13,9 +13,9 @@ public class RuleBaseSystem {
 	} else {
 	    fm = new FileManager();
 	    ArrayList<Rule> rules = fm.loadRules("CarShop.data");
-	    //ArrayList rules = fm.loadRules("AnimalWorld.data");
+	    //ArrayList<Rule> rules = fm.loadRules("AnimalWorld.data");
 	    ArrayList<String> wm    = fm.loadWm("CarShopWm.data");
-	    //ArrayList wm    = fm.loadWm("AnimalWorldWm.data");
+	    //ArrayList<String> wm    = fm.loadWm("AnimalWorldWm.data");
 	    rb = new RuleBase(rules,wm);
 	    StringTokenizer st = new StringTokenizer(args[0],",");
 	    ArrayList<String> queries = new ArrayList<String>();
